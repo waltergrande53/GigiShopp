3d1053fd897d58d02c0a6453ccf11224009bf308
waltergrande53 <59238468+waltergrande53@users.noreply.github.com>
2021-09-20 22:51:55 -0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import '/styles/globals.css'
-import Head from 'next/head'
+
 
 // own css files hereim
 
@@ -9,24 +9,8 @@ function MyApp({ Component, pageProps }) {
   return ( 
     
   <Layout>
-    <Head>
  
-<link
-  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"
-  rel="stylesheet"
-/>
-
-<link
-  href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-  rel="stylesheet"
-/>
-
-<link
-  href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.6.0/mdb.min.css"
-  rel="stylesheet"
-/>
-
-    </Head>
+  
     <Component {...pageProps}/> 
     
     </Layout>
